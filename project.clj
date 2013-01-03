2c3cee5ee5eaed351e31d8f9be008b373eba91ae
Geoff Shannon <earthlingzephyr@gmail.com>
2013-01-02 22:47:04 -0800

    Start adding dependencies and structure for generating the xml.

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject lein-launch4j "0.1.1-SNAPSHOT"
+(defproject lein-launch4j "0.1.1"
   :description "wrap your project with launch4j"
   :url "https://github.com/ezephyr/lein-launch4j"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
-  :dependencies [[launch4j "3.0.2"]])
+  :dependencies [[launch4j "3.0.2"]
+                 [org.clojure/data.xml "0.0.6"]])
